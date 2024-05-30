9a190642331cc0bda1a4f7bf5e5253d06dcf690f
Jean-Pierre Chauvel <jean.p.chauvel@gmail.com>
2024-05-30 15:52:49 +0000

    WIP Linux

--- run_form.py
+++ run_form.py
@@ -2,21 +2,24 @@
 import asyncio
 from datetime import datetime
 from functools import lru_cache, wraps
+import math
 from pathlib import Path
 
 import aiocsv
 import aiofiles
-import typer
+from nordvpn_switcher import initialize_VPN, rotate_VPN, terminate_VPN
 from playwright.async_api import (
     Browser,
     BrowserContext,
     Page,
+    TimeoutError,
     async_playwright,
 )
 from playwright_stealth import stealth_async
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from rand_useragent import randua
 from rich.progress import track
+import typer
 from typing_extensions import Annotated
 from undetected_playwright import Malenia
 from unicaps import AsyncCaptchaSolver, CaptchaSolvingService
@@ -32,11 +35,14 @@ FIELDS = [
     "defendant",
 ]
 
+workers: list[asyncio.Task[None]] = []
+
 
 class Settings(BaseSettings):
     anti_captcha_api_key_path: str = ""
     url: str = ""
     since: int = 0
+    threshold: float = .0
 
     model_config = SettingsConfigDict(env_file=".env")
 
@@ -123,143 +129,209 @@ async def save_dict_to_csv(row: dict[str, str], csv_file) -> None:
 
 async def worker(
     incoming_queue: asyncio.Queue[tuple[str, str, str]],
+    cancelled_workers_queue: asyncio.Queue[bool],
     progress_bar_queue: asyncio.Queue[bool],
 ) -> None:
     while True:
         document_number, year, output_file = await incoming_queue.get()
         settings: Settings = get_settings()
         async with async_playwright() as p:
-            browser: Browser = await p.firefox.launch()
-
-            # Create  a new context and page
-            context: BrowserContext = await browser.new_context(
-                user_agent=randua()
-            )
-            await Malenia.apply_stealth(context)
-
-            page: Page = await context.new_page()
-
-            # Apply the stealth settings
-            await stealth_async(page)
-
-            # Navigate to the page
-            await page.goto(settings.url)
-
-            # Wait for the webpage to load completely
-            await page.wait_for_load_state("load")
-
-            await page.locator("#distritoJudicial").select_option("LIMA")
-
-            await page.wait_for_function(
-                """
-                document.querySelector('#organoJurisdiccional option[value="16133"]')
-            """
-            )
-            await page.locator("#organoJurisdiccional").select_option(
-                "JUZGADO DE PAZ LETRADO"
-            )
-
-            await page.wait_for_function(
-                """
-                document.querySelector('#especialidad option[value="97880"]')
-            """
-            )
-            await page.locator("#especialidad").select_option("CIVIL")
-
-            await page.locator("#anio").select_option(year)
-
-            await page.locator("#numeroExpediente").fill(document_number)
-
-            await page.wait_for_selector("#captcha_image")
-
-            async with aiofiles.tempfile.NamedTemporaryFile() as tmpfile:
-                await page.locator("#captcha_image").screenshot(
-                    path=tmpfile.name
+            browser: Browser = await p.firefox.launch(headless=False)
+            try:
+                # Create  a new context and page
+                context: BrowserContext = await browser.new_context(
+                    user_agent=randua()
                 )
-                async with AsyncCaptchaSolver(
-                    CaptchaSolvingService.ANTI_CAPTCHA,
-                    settings.anti_captcha_api_key,
-                ) as solver:
-                    solved = await solver.solve_image_captcha(
-                        Path(tmpfile.name),
-                        is_phrase=False,
-                        is_case_sensitive=False,
+                await Malenia.apply_stealth(context)
+
+                page: Page = await context.new_page()
+
+                # Apply the stealth settings
+                await stealth_async(page)
+
+                # Navigate to the page
+                await page.goto(settings.url)
+
+                try:
+                    # Wait for the webpage to load completely
+                    await page.wait_for_load_state("load")
+
+                    await page.locator("#distritoJudicial").select_option(
+                        "LIMA"
                     )
-                    await page.locator("#codigoCaptcha").fill(
-                        solved.solution.text
+
+                    await page.wait_for_function(
+                        """
+                        document.querySelector('#organoJurisdiccional option[value="16133"]')
+                    """
                     )
-                    await solved.report_good()
+                    await page.locator("#organoJurisdiccional").select_option(
+                        "JUZGADO DE PAZ LETRADO"
+                    )
 
-            await page.locator("#consultarExpedientes").click()
+                    await page.wait_for_function(
+                        """
+                        document.querySelector('#especialidad option[value="97880"]')
+                    """
+                    )
+                    await page.locator("#especialidad").select_option("CIVIL")
 
-            await asyncio.sleep(1)
+                    await page.locator("#anio").select_option(year)
 
-            # Wait for the webpage to load completely
-            await page.wait_for_load_state("load")
+                    await page.locator("#numeroExpediente").fill(
+                        document_number
+                    )
 
-            buttons: list = await page.locator("div#divDetalles button").all()
-            buttons_length: int = len(buttons)
-            i: int = 0
-            while i < buttons_length:
-                if (
-                    await buttons[i].get_attribute("title")
-                    == "Ver detalle de expediente"
-                ):
-                    await buttons[i].click()
+                    await page.wait_for_selector("#captcha_image")
+
+                    async with aiofiles.tempfile.NamedTemporaryFile() as tmpfile:
+                        await page.locator("#captcha_image").screenshot(
+                            path=tmpfile.name
+                        )
+                        async with AsyncCaptchaSolver(
+                            CaptchaSolvingService.ANTI_CAPTCHA,
+                            settings.anti_captcha_api_key,
+                        ) as solver:
+                            solved = await solver.solve_image_captcha(
+                                Path(tmpfile.name),
+                                is_phrase=False,
+                                is_case_sensitive=False,
+                            )
+                            await page.locator("#codigoCaptcha").fill(
+                                solved.solution.text
+                            )
+                            await solved.report_good()
+
+                    await page.locator("#consultarExpedientes").click()
 
                     await asyncio.sleep(1)
 
                     # Wait for the webpage to load completely
                     await page.wait_for_load_state("load")
 
-                    header_content: str | None = await page.locator(
-                        "div#gridRE"
-                    ).text_content()
-                    parties_content: str | None = await page.locator(
-                        "div#collapseTwo"
-                    ).text_content()
-
-                    if (
-                        header_content is not None
-                        and parties_content is not None
-                    ):
-                        cleaned_header: list[str] = clean_header(
-                            header_content
-                        )
-                        cleaned_parties: list[str] = clean_parties(
-                            parties_content
-                        )
-                        data: list[str] = cleaned_header + cleaned_parties
-                        converted_data: dict[str, str] = convert_data_to_dict(
-                            data
-                        )
-                        await save_dict_to_csv(converted_data, output_file)
-
-                    await asyncio.sleep(1)
-
-                    anchors: list = await page.locator("div#divCuerpo a").all()
-                    await anchors[0].click()
-
-                    await asyncio.sleep(1)
-
-                    # Wait for the webpage to load completely
-                    await page.wait_for_load_state("load")
                     buttons: list = await page.locator(
                         "div#divDetalles button"
                     ).all()
                     buttons_length: int = len(buttons)
-                    i += 1
+                    i: int = 0
+                    while i < buttons_length:
+                        if (
+                            await buttons[i].get_attribute("title")
+                            == "Ver detalle de expediente"
+                        ):
+                            await buttons[i].click()
 
-            # Close the browser
-            await browser.close()
-            await progress_bar_queue.put(True)
-            incoming_queue.task_done()
+                            await asyncio.sleep(1)
+
+                            # Wait for the webpage to load completely
+                            await page.wait_for_load_state("load")
+
+                            header_content: str | None = await page.locator(
+                                "div#gridRE"
+                            ).text_content()
+                            parties_content: str | None = await page.locator(
+                                "div#collapseTwo"
+                            ).text_content()
+
+                            if (
+                                header_content is not None
+                                and parties_content is not None
+                            ):
+                                cleaned_header: list[str] = clean_header(
+                                    header_content
+                                )
+                                cleaned_parties: list[str] = clean_parties(
+                                    parties_content
+                                )
+                                data: list[str] = (
+                                    cleaned_header + cleaned_parties
+                                )
+                                converted_data: dict[
+                                    str, str
+                                ] = convert_data_to_dict(data)
+                                await save_dict_to_csv(
+                                    converted_data, output_file
+                                )
+
+                            await asyncio.sleep(1)
+
+                            anchors: list = await page.locator(
+                                "div#divCuerpo a"
+                            ).all()
+                            await anchors[0].click()
+
+                            await asyncio.sleep(1)
+
+                            # Wait for the webpage to load completely
+                            await page.wait_for_load_state("load")
+
+                            buttons: list = await page.locator(
+                                "div#divDetalles button"
+                            ).all()
+                            buttons_length: int = len(buttons)
+                            i += 1
+
+                    await progress_bar_queue.put(True)
+                    incoming_queue.task_done()
+
+                except TimeoutError:
+                    # Close the browser
+                    await browser.close()
+                    await cancelled_workers_queue.put(True)
+                    await asyncio.sleep(.5)
+            finally:
+                if browser.is_connected():
+                    # Close the browser
+                    await browser.close()
 
 
 async def progress_bar(queue: asyncio.Queue[bool], steps: int) -> None:
     for _ in track(range(steps)):
         await queue.get()
         queue.task_done()
+
+
+async def respawn_workers(
+    vpn_settings: dict,
+    num_workers: int,
+    incoming_queue: asyncio.Queue[tuple[str, str, str]],
+    cancelled_workers_queue: asyncio.Queue[bool],
+    progress_bar_queue: asyncio.Queue[bool],
+) -> None:
+    global workers
+    settings: Settings = get_settings()
+    count: int = num_workers
+    threshold: int = math.ceil(num_workers * settings.threshold)
+    while True:
+        while count > threshold:
+            await cancelled_workers_queue.get()
+            count -= 1
+        for task in workers:
+            task.cancel()
+        await asyncio.gather(*workers, return_exceptions=True)
+        rotate_VPN(vpn_settings)
+        workers = []
+        for _ in range(num_workers):
+            worker_task = create_worker(
+                incoming_queue=incoming_queue,
+                cancelled_workers_queue=cancelled_workers_queue,
+                progress_bar_queue=progress_bar_queue,
+            )
+            workers.append(worker_task)
+
+
+def create_worker(
+    incoming_queue: asyncio.Queue[tuple[str, str, str]],
+    cancelled_workers_queue: asyncio.Queue[bool],
+    progress_bar_queue: asyncio.Queue[bool],
+) -> asyncio.Task[None]:
+    return asyncio.create_task(
+        coro=worker(
+            incoming_queue=incoming_queue,
+            cancelled_workers_queue=cancelled_workers_queue,
+            progress_bar_queue=progress_bar_queue,
+        )
+    )
 
 
 def get_year(year: str) -> int:
@@ -282,29 +354,53 @@ async def main(
     ] = "output.csv",
     num_workers: Annotated[
         int, typer.Option(help="The number of workers")
-    ] = 5
+    ] = 5,
 ) -> None:
+    global workers
+    vpn_settings = initialize_VPN(
+        stored_settings=0,
+        save=0,
+        area_input=["Chile"],
+        skip_settings=1,
+    )
+    rotate_VPN(vpn_settings)
     await write_header_to_csv(output_file)
     settings: Settings = get_settings()
 
     now = get_year(until_year)
     incoming_queue: asyncio.Queue[tuple[str, str, str]] = asyncio.Queue()
     progress_bar_queue: asyncio.Queue[bool] = asyncio.Queue()
+    cancelled_workers_queue: asyncio.Queue[bool] = asyncio.Queue()
 
     for year in range(settings.since, now + 1):
         for i in range(document_range):
             incoming_queue.put_nowait(
                 (str(document_number_start + i), str(year), output_file)
             )
+
     tasks: list[asyncio.Task[None]] = []
-    for _ in range(num_workers):
-        task: asyncio.Task[None] = asyncio.create_task(
-            coro=worker(
-                incoming_queue=incoming_queue,
-                progress_bar_queue=progress_bar_queue,
-            )
+
+    # Start the respawn workers task
+    respawn_workers_task: asyncio.Task[None] = asyncio.create_task(
+        coro=respawn_workers(
+            vpn_settings=vpn_settings,
+            num_workers=num_workers,
+            incoming_queue=incoming_queue,
+            cancelled_workers_queue=cancelled_workers_queue,
+            progress_bar_queue=progress_bar_queue,
         )
-        tasks.append(task)
+    )
+    tasks.append(respawn_workers_task)
+
+    # Start workers
+    for _ in range(num_workers):
+        worker_task: asyncio.Task[None] = create_worker(
+            incoming_queue=incoming_queue,
+            cancelled_workers_queue=cancelled_workers_queue,
+            progress_bar_queue=progress_bar_queue,
+        )
+        workers.append(worker_task)
+        tasks.append(worker_task)
 
     # Start progress bar
     progress_bar_task: asyncio.Task[None] = asyncio.create_task(
@@ -315,8 +411,9 @@ async def main(
     )
     tasks.append(progress_bar_task)
 
-    # Wait until the queue is fully processed
+    # Wait until the queues are fully processed
     await incoming_queue.join()
+    await cancelled_workers_queue.join()
     await progress_bar_queue.join()
 
     for task in tasks:
@@ -324,6 +421,7 @@ async def main(
 
     # Wait until all tasks are cancelled
     await asyncio.gather(*tasks, return_exceptions=True)
+    terminate_VPN(vpn_settings)
 
 
 if __name__ == "__main__":
